5379aec121f717af016703ce6bebcbbcf49ab5e9
S.Pavit <skynasa2014@gmail.com>
2026-04-22 23:31:07 +0700

    Update App.js

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ export default function App() {
   );
 
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style = {{flex : 1}}>
     <NavigationContainer>
       <AuthProvider>
         <RootNavigator />
